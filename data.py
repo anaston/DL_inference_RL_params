b2eb31ac1b7ec67304e876637310c4fc253db326
Agoston Mihalik <anaston@gmail.com>
2023-12-04 12:43:21 +0000

    Refactor codebase for flexible RNN training

--- data.py
+++ data.py
@@ -4,13 +4,15 @@ import pandas as pd
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
+from abc import abstractmethod
 
 
-class UnlabeledDataset(Dataset):
+class BaseDataset(Dataset):
     """
-    Unlabeled dataset of synthetic data
+    Class for abstract dataset of synthetic data
 
-    NB: dtype of tensors is essential for the model to work later on
+    NB: dataset is assumed to bo complete, i.e., all unique values are assumed
+    to appear each variable
     """
     def __init__(self, df=None, path=None):
         """Initialize dataset"""
@@ -25,61 +27,145 @@ class UnlabeledDataset(Dataset):
 
         # Agents
         self.agent = self.df['agent'].unique()
+        self.nagents = len(self.agent)
 
-        # Number of actions
-        self.nactions = self.df['action'].nunique()
+        # Number of blocks
+        self.nblocks = self.df['block'].nunique()
+
+        # Number of trials
+        self.ntrials = self.df['trial'].nunique()
+
+        # Number of bins for alpha and beta (remove NaNs!)
+        try:
+            self.alpha_nbins = self.df['alpha_bin'].dropna().nunique()
+            self.beta_nbins = self.df['beta_bin'].dropna().nunique()
+        except KeyError:  # for compatibility with old datasets
+            self.alpha_nbins = 5
+            self.beta_nbins = 5
+
+        # Initialize variables
+        self.vars = dict()
+
+    @abstractmethod
+    def __getitem__(self, idx):
+        """Get agents as dataset items"""
+        pass
+
+    def __len__(self):
+        """Define size of dataset as number of agents"""
+        return self.agent.size
+
+    def _set_vars(self, vars, encoding={'dummy': ['action', 'offer']},
+                  dtype={torch.int64: ['alpha_bin', 'beta_bin'],
+                         torch.float32: ['action', 'reward', 'offer', 'alpha', 'beta']},
+                  shape={(-1, 1): ['alpha', 'beta', 'reward']}):
+        """Set attributes of dataset variables"""
+        for i in vars:
+            if i not in self.vars:
+                # Initialize dictionary
+                self.vars[i] = dict()
+
+                # Set attributes
+                attributes = {'encoding': encoding, 'dtype': dtype,
+                              'shape': shape}
+                for key in attributes:
+                    for subkey, value in attributes[key].items():
+                        if i in value:
+                            self.vars[i][key] = subkey
+
+        # Shape of the last dimension of variables
+        for i in vars:
+            if i in encoding['dummy']:
+                # Dummy variable
+                self.vars[i]['last_shape'] = np.unique(
+                    self.df.loc[:, self.df.columns.str.startswith(i)].values).size
+            else:
+                # Other variable
+                self.vars[i]['last_shape'] = 1
+
+    def _var2tensor(self, df, varname):
+        """Convert input/output variable to tensor"""
+        # Get variable as numpy array
+        if 'encoding' in self.vars[varname] \
+                and self.vars[varname]['encoding'] == 'dummy':
+            # Dummy variable
+            var = np.zeros((len(df), self.vars[varname]['last_shape']))
+            is_var = df.columns.str.startswith(varname)
+            var[np.repeat(np.arange(len(df)), is_var.sum()), df.loc[
+                :, is_var].values.flat] = 1
+        else:
+            # Other variable
+            var = df[varname].values
+
+        # Convert to tensor
+        var = torch.tensor(var, dtype=self.vars[varname]['dtype'])
+
+        # Reshape variable
+        if 'shape' in self.vars[varname]:
+            var = var.reshape(*self.vars[varname]['shape'])
+
+        # Return variable as tensor
+        return var
+
+
+class UnlabeledDataset(BaseDataset):
+    """
+    Unlabeled dataset of synthetic data
+    """
+    def __init__(self, inputs, **kwargs):
+        """Initialize dataset"""
+        # Initialize parent class
+        super().__init__(**kwargs)
+
+        # Inputs - check if they are in dataframe
+        self.inputs = inputs
+        for i in inputs:
+            if i not in self.df.columns:
+                raise ValueError('Unknown input')
+
+        # Set attributes of inputs
+        self._set_vars(inputs)
+
+        # Number of inputs
+        self.ninputs = sum([self.vars[i]['last_shape'] for i in inputs])
 
     def __getitem__(self, idx):
         """Get agents as dataset items"""
-        # Get dataframe for agent
+        # Get dataframe for list of agents
         df = self.df[self.df['agent'] == self.agent[idx]]
 
-        # Number of blocks
-        nblocks = df['block'].nunique()
+        # Convert inputs to tensors and stack them
+        X = torch.hstack([self._var2tensor(df, i) for i in self.inputs])
 
-        # Convert action to one-hot encoded tensor
-        action_onehot = nn.functional.one_hot(
-            torch.from_numpy(df['action'].values),
-            self.nactions).type(dtype=torch.float32)
+        # Add shift to input data
+        X = nn.functional.pad(X, [0, 0, 1, 0], 'constant', value=0)[:-1]
 
-        # Reward as tensor
-        reward = torch.tensor(df['reward'].values,
-                              dtype=torch.float32)[:, np.newaxis]
-
-        # Input data (with dummy zeros in the beginning)
-        # TODO: check if first step is necessary as there appears to be
-        # block-wise padding in the second step
-        X = nn.functional.pad(torch.hstack((reward, action_onehot)),
-                              [0, 0, 1, 0], 'constant', value=0)[:-1]
-        X.reshape(nblocks, -1, X.shape[1])[:, 0, :] = (
-            torch.zeros(size=(nblocks, X.shape[1]))
+        # Add dummy zeros to the beginning of each block
+        X.reshape(self.nblocks, -1, X.shape[1])[:, 0, :] = (
+            torch.zeros(size=(self.nblocks, X.shape[1]))
         )
 
         # Return input data
         return X
-
-    def __len__(self):
-        """Define size of dataset as number of agents"""
-        return len(self.agent)
 
 
 class LabeledDataset(UnlabeledDataset):
     """
     Labeled dataset of synthetic data
     """
-    def __init__(self, labels, df=None, path=None):
+    def __init__(self, inputs, outputs, **kwargs):
         """Initialize dataset"""
         # Initialize parent class
-        super().__init__(df=df, path=path)
+        super().__init__(inputs, **kwargs)
 
-        # Labels
-        self.labels = labels
+        # Outputs - check if they are in dataframe
+        self.outputs = outputs
+        for i in outputs:
+            if i not in self.df.columns:
+                raise ValueError('Unknown output')
 
-        # Number of bins for alpha and beta
-        if 'alpha_bin' in labels:
-            self.nbins_alpha = self.df['alpha_bin'].nunique()
-        if 'beta_bin' in labels:
-            self.nbins_beta = self.df['beta_bin'].nunique()
+        # Set attributes of outputs
+        self._set_vars(outputs)
 
     def __getitem__(self, idx):
         """Get agents as dataset items"""
@@ -89,32 +175,8 @@ class LabeledDataset(UnlabeledDataset):
         # Get dataframe for agent
         df = self.df[self.df['agent'] == self.agent[idx]]
 
-        # Multi-output data
-        # NB: tensors must be in specific shapes to match those of model
-        # outputs and/or requirements for loss functions in rnn.multi_loss
-        # It is also critical that the order of labels must match the
-        # order in rnn.multi_loss
-        y = []
-        for label in self.labels:
-            if label == 'action':
-                if df['action'].nunique() == 1:
-                    y.append(torch.from_numpy(
-                        np.repeat(df['action'].values, self.nactions).reshape(
-                            -1, self.nactions).type(dtype=torch.float32)))
-                else:
-                    y.append(nn.functional.one_hot(
-                        torch.from_numpy(df['action'].values),
-                        self.nactions).type(dtype=torch.float32),)
-            elif label in ['alpha_bin', 'beta_bin']:
-                y.append(torch.tensor(df[label].values, dtype=torch.int64))
-            elif label in ['alpha', 'beta']:
-                y.append(torch.tensor(df[label].values,
-                                      dtype=torch.float32).reshape(-1, 1))
-            else:
-                raise ValueError('Unknown label')
-        
-        return X, y
+        # Convert outputs to tensors
+        # NB: we keep outputs in list to be able to use specific dtype for each
+        y = [self._var2tensor(df, o) for o in self.outputs]
 
-    def __len__(self):
-        """Define size of dataset as number of agents"""
-        return len(self.agent)
+        return X, y
--- rnn.py
+++ rnn.py
@@ -3,8 +3,40 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+from torch.utils.data import DataLoader, Subset
+from sklearn.model_selection import train_test_split
 import numpy as np
 import os
+
+
+class Loss:
+    """
+    Class for custom loss function
+    """
+    def __init__(self, name, weight, tol=0):
+        """Initialize loss function"""
+        # Name of loss function
+        self.name = name
+
+        # Loss function
+        self.func = getattr(nn, f'{self.name}Loss')()
+
+        # Relative weight of loss when calculating multi-loss
+        self.weight = weight
+
+        # Tolerance for numerical stability
+        self.tol = tol
+
+    def __call__(self, y_pred, y_true):
+        """Compute loss"""
+        if self.name == 'CrossEntropy':
+            # Reshape y_pred to match y_true
+            y_pred = torch.transpose(y_pred, 1, 2)
+        elif self.name == 'BCE':
+            # Add tolerance to predictions
+            y_pred += self.tol
+
+        return self.func(y_pred, y_true) * self.weight
 
 
 class GRU(nn.Module):
@@ -12,94 +44,93 @@ class GRU(nn.Module):
     Class for custom RNN model with two GRU layers
 
     NB: we keep the naming convention of layers from the original paper
-    to be able to compare with the original implementation (but new
+    to be able to compare the results with the original implementation (but new
     suggestinos are made in the comments for the future)
     """
-    def __init__(self, input_size, hidden_size, alpha_embedding_size,
-                 beta_embedding_size, output_size, dropout,):
+    def __init__(self, ds, hidden_size, dropout,):
         """Initialize model"""
         # Initialize parent class
         super().__init__()
 
-        # Set size attributes
-        self.input_size = input_size
-        self.alpha_embedding_size = alpha_embedding_size
-        self.beta_embedding_size = beta_embedding_size
+        # Initialize dictionary for model output
+        self.y = dict()
+
+        # Initialize each loss of multi-loss function
+        self.loss = {'action': Loss('BCE', weight=1),
+                     'alpha_bin': Loss('CrossEntropy', weight=0.2),
+                     'beta_bin': Loss('CrossEntropy', weight=0.2),
+                     'alpha': Loss('MSE', weight=6),
+                     'beta': Loss('MSE', weight=0.1)}
+        self.loss = {k: v for k, v in self.loss.items() if k in ds.outputs}
 
         # GRU layer on inputs (self.gru_input instead of self.hidden_0?)
-        self.hidden_0 = nn.GRU(input_size=input_size, hidden_size=hidden_size,
-                               num_layers=1, batch_first=True,
-                               dropout=dropout,)
+        self.hidden_0 = nn.GRU(input_size=ds.ninputs,
+                               hidden_size=hidden_size, num_layers=1,
+                               batch_first=True, dropout=dropout,)
 
         # Linear layers on GRU output (self.linear_*_bin instead of
         # self.out_*?)
-        self.out_alpha = nn.Linear(hidden_size, alpha_embedding_size)
-        self.out_beta = nn.Linear(hidden_size, beta_embedding_size)
+        self.out_alpha = nn.Linear(hidden_size, ds.alpha_nbins)
+        self.out_beta = nn.Linear(hidden_size, ds.beta_nbins)
 
         # RELU layers on linear output
         self.relu_alpha = nn.ReLU()
         self.relu_beta = nn.ReLU()
 
         # Linear layers on RELU output (self.linear_* instead of self.reg_*?)
-        self.reg_alpha = nn.Linear(alpha_embedding_size, 1)
-        self.reg_beta = nn.Linear(beta_embedding_size, 1)
+        self.reg_alpha = nn.Linear(ds.alpha_nbins, 1)
+        self.reg_beta = nn.Linear(ds.beta_nbins, 1)
 
         # GRU layer on embedded inputs (self.gru_embedded instead of
         # self.hidden_1?)
-        self.hidden_1 = nn.GRU(input_size=input_size
-                               + alpha_embedding_size
-                               + beta_embedding_size,
+        self.hidden_1 = nn.GRU(input_size=ds.ninputs
+                               + ds.alpha_nbins + ds.beta_nbins,
                                hidden_size=hidden_size, num_layers=1,
                                batch_first=True, dropout=dropout,)
 
         # Linear layer on embedded GRU output (self.linear_action instead of
         # self.out_action?)
-        self.out_action = nn.Linear(hidden_size, output_size)
+        self.out_action = nn.Linear(
+            hidden_size, ds.vars['action']['last_shape'])
 
     def forward(self, X):
-        """Forward pass model"""
+        """Forward pass model"""        
         # Predict hidden estimates using GRU layer
         y_hidden, state_hidden = self.hidden_0(X)
 
         # Predict binary alpha and beta estimates using linear and RELU layers
-        y_alpha_bin = self.relu_alpha(self.out_alpha(y_hidden))
-        y_beta_bin = self.relu_beta(self.out_beta(y_hidden))
+        self.y['alpha_bin'] = self.relu_alpha(self.out_alpha(y_hidden))
+        self.y['beta_bin'] = self.relu_beta(self.out_beta(y_hidden))
 
         # Predict alpha and beta estimates using linear layer
-        y_alpha = self.reg_alpha(y_alpha_bin)
-        y_beta = self.reg_beta(y_beta_bin)
+        self.y['alpha'] = self.reg_alpha(self.y['alpha_bin'])
+        self.y['beta'] = self.reg_beta(self.y['beta_bin'])
 
         # Create embedded input
-        X_embedded = torch.cat((X, y_alpha_bin, y_beta_bin), dim=2)
+        X_embedded = torch.cat((X, self.y['alpha_bin'], self.y['beta_bin']),
+                               dim=2)
 
         # Predict action using GRU layer
         y_action, state_action = self.hidden_1(X_embedded)
-        y_action = F.softmax(self.out_action(y_action), dim=-1)
+        self.y['action'] = F.softmax(self.out_action(y_action), dim=-1)
 
-        return (y_action, y_alpha_bin, y_beta_bin, y_alpha, y_beta,
-                state_hidden, state_action,)
+        return (self.y['action'], self.y['alpha_bin'], self.y['beta_bin'],
+                self.y['alpha'], self.y['beta'], state_hidden, state_action,)
 
-
-def multi_loss(y_action, y_alpha_bin, y_beta_bin, y_alpha, y_beta):
-    """Multi-loss function"""
-    # Compute individual losses
-    # TODO: check dimensions and order of inputs
-    loss = [nn.BCELoss()(*y_action),
-            nn.CrossEntropyLoss()(*y_alpha_bin),
-            nn.CrossEntropyLoss()(*y_beta_bin),
-            nn.MSELoss()(*y_alpha),
-            nn.MSELoss()(*y_beta)]
-
-    # Return weighted losses
-    loss_weights = [1, 0.2, 0.2, 6, 0.1]
-    return [loss_weights[i] * val for i, val in enumerate(loss)]
+    def calc_loss(self, y_true):
+        """Calculate each loss of multi-loss function"""
+        # Calculate losses
+        return [self.loss[val](self.y[val], y_true[i])
+                for i, val in enumerate(self.loss.keys())]
 
 
-def train_one_epoch(model, device, optimizer, train_loader):
+def train_one_epoch(model, device, optimizer, train_ds):
     """Train RNN model for one epoch"""
     # Initialize running loss as array
-    # TODO: avoid hard-coding dimension of multi-loss
-    running_loss = np.zeros(5)
+    running_loss = np.zeros(len(model.loss))
+
+    # Instantiate Data Loader for traning data
+    train_loader = DataLoader(train_ds, shuffle=False, batch_size=1)
 
     # Loop over training data
     for X, y_true in train_loader:
@@ -111,16 +142,10 @@ def train_one_epoch(model, device, optimizer, train_loader):
         optimizer.zero_grad()
 
         # Forward pass
-        y_action, y_alpha_bin, y_beta_bin, y_alpha, y_beta, _, _ = model(X)
+        model(X)
 
-        # Compute loss
-        # NB: input dimensions need to be transposed for CE loss
-        # TODO: automate the order of y_true
-        loss = multi_loss((y_action, y_true[0]),
-                          (torch.transpose(y_alpha_bin, 1, 2), y_true[1]),
-                          (torch.transpose(y_beta_bin, 1, 2), y_true[2]),
-                          (y_alpha, y_true[3]),
-                          (y_beta, y_true[4]),)
+        # Calculate loss
+        loss = model.calc_loss(y_true)
 
         # Backpropagate loss
         sum(loss).backward()
@@ -134,11 +159,13 @@ def train_one_epoch(model, device, optimizer, train_loader):
     return model, optimizer, running_loss / len(train_loader)
 
 
-def eval_model(model, device, val_loader):
+def eval_model(model, device, val_ds):
     """Evaluate RNN model"""
     # Initialize running loss
-    # TODO: avoid hard-coding dimension of multi-loss
-    running_loss = np.zeros(5)
+    running_loss = np.zeros(len(model.loss))
+
+    # Instantiate Data Loader for validation data
+    val_loader = DataLoader(val_ds, shuffle=False, batch_size=1)
 
     # Loop over validation data
     for X, y_true in val_loader:
@@ -147,24 +174,23 @@ def eval_model(model, device, val_loader):
         y_true = [y.to(device) for y in y_true]
 
         # Forward pass
-        y_action, y_alpha_bin, y_beta_bin, y_alpha, y_beta, _, _ = model(X)
+        model(X)
 
-        # Compute loss
-        # NB: input dimensions need to be transposed for CE loss
-        loss = multi_loss((y_action, y_true[0]),
-                          (torch.transpose(y_alpha_bin, 1, 2), y_true[1]),
-                          (torch.transpose(y_beta_bin, 1, 2), y_true[2]),
-                          (y_alpha, y_true[3]),
-                          (y_beta, y_true[4]),)
+        # Calculate loss
+        loss = model.calc_loss(y_true)
+
         running_loss += [val.item() for val in loss]
 
     return running_loss / len(val_loader)
 
 
 # Training loop
-def training_loop(model, device, train_loader, val_loader,
-                  fname, nepochs=100, optimizer_lr=0.001):
+def training_loop(ds, model, device, nepochs=100, fname='',
+                  optimizer_lr=0.001):
     """Training loop"""
+    # Training and validation indices
+    train_idx, val_idx = train_test_split(range(len(ds)), test_size=0.2)
+
     # Initialize minimum losses (the values are arbitrarily large)
     min_train_loss = 100
     min_val_loss = 100
@@ -182,22 +208,20 @@ def training_loop(model, device, train_loader, val_loader,
     for i in range(nepochs):
         # Train one epoch
         model, optimizer, loss = train_one_epoch(model, device, optimizer,
-                                                 train_loader)
+                                                 Subset(ds, train_idx))
         train_loss.append(sum(loss))
 
         # Print losses
         print("================")
-        loss_names = ["BCE action", "CE alpha", "CE beta", "MSE alpha",
-                      "MSE beta"]
-        for j, val in enumerate(loss):
-            print(f"loss {loss_names[j]}: {val}")
+        for j, val in enumerate(model.loss.keys()):
+            print(f"loss {model.loss[val].name} {val}: {loss[j]}")
 
         with torch.no_grad():
             # Change to evaluation mode
             model.eval()
 
             # Evaluate model
-            loss = eval_model(model, device, val_loader)
+            loss = eval_model(model, device, Subset(ds, val_idx))
             val_loss.append(sum(loss))
 
         if train_loss[i] <= min_train_loss:
